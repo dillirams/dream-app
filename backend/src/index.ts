871e54d5534690cd3be80939df25f389f7227348
Your Name <dilliram2003adhikari@gmail.com>
2026-02-03 15:40:48 +0530

    added api in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,13 +5,13 @@ import { appRouter } from './route/route.js';
 
 const app=express();
 app.use(cors({
-   origin:['http://localhost:5173','http://localhost:3000','https://dream.dillaxadhi.com'],
+   origin:['http://localhost:5173','http://localhost:3000','https://dream.dillaxadhi.com','http://dream.dillaxadhi.com'],
    credentials:true
 }))
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/user',appRouter)
+app.use('/api/user',appRouter)
 app.listen(3000,()=>{
     console.log("the app is listeninig to port 3000");
 })
